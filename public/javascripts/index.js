30a4db32e3a1c74c377b3ce7117180161b71a031
YinHao <shdic2@163.com>
2024-12-11 19:22:27 +0800

    add

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -19,7 +19,8 @@ let editor = null,
     ".heic",
     ".avif",
   ];
-
+const vsCodes = document.querySelectorAll(".vs-code");
+const openSite = document.querySelectorAll(".open-site");
 function isImageFile(filepath) {
   const ext = "." + filepath.split(".").pop().toLowerCase();
   return imageExtensions.includes(ext);
@@ -216,8 +217,6 @@ const createContent = (
       <a href="javascript:" class="nav_a dey-to-pro" role="button" data-lan="${lan}" style="display: none">上传至 ${lan} Pro Ftp</a>
       <a href="javascript:" class="nav_a all-img" style="display: ${selectLan === lan ? "none" : ""}" title="Resource Async" role="button" data-lan="${lan}"><svg t="1729820839670" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="19174" width="30" height="30"><path d="M636.330667 133.717333c99.072 9.898667 178.773333 69.674667 195.669333 172.117334h-57.344l92.672 137.984L960 305.834667h-63.914667c-17.066667-137.984-121.429333-227.626667-253.226666-240.426667-18.218667-1.408-35.157333 14.208-35.157334 34.133333 1.194667 17.066667 12.885333 32.725333 28.586667 34.133334zM450.133333 64H127.914667c-18.261333 0-32.554667 15.616-32.554667 35.584v307.2c0 19.968 14.293333 35.584 32.554667 35.584h322.261333c18.218667 0 32.554667-15.616 32.554667-35.584v-307.2c0-19.968-14.336-35.584-32.554667-35.584z m-31.36 310.101333H159.274667V132.266667H418.986667l-0.128 241.834666zM387.413333 888.96c-110.933333-9.898667-195.712-83.882667-199.68-206.250667h61.312l-92.416-137.813333L64 682.666667h61.312c2.474667 157.909333 113.408 260.352 255.701333 274.56 18.218667 1.408 35.157333-14.208 35.157334-34.133334a33.877333 33.877333 0 0 0-28.714667-34.133333z m509.824-308.650667h-322.261333c-18.346667 0-32.554667 15.786667-32.554667 35.584v308.650667c0 19.84 14.293333 35.413333 32.554667 35.413333h322.261333c18.218667 0 32.554667-15.573333 32.554667-35.413333v-308.650667c0-19.84-15.616-35.584-32.554667-35.584z m-31.402666 310.101334H606.293333v-240.384h259.626667v240.384z" p-id="19175" fill="#ffffff"></path></svg></a>
       <a href="javascript:" class="nav_a publish" title="Publish" role="button" data-lan="${lan}"><svg t="1729820784129" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="18059" width="30" height="30"><path d="M939.52 22.752L28.448 392.256l173.792 164.416 53.76-55.456-86.464-81.92L783.648 170.112 173.504 750.368h-1.984V1024h76.224v-237.792l115.488-109.504 55.744-53.184L937.248 131.136l-86.752 89.6-94.432 319.712L655.648 879.232l-118.912-112.64-53.76 55.456 210.496 199.104 224.416-759.168L995.552 0l-56.032 22.752z" p-id="18060" fill="#ffffff"></path></svg></a>
-      <a href="javascript:" class="nav_a vs-code" title="Vs Code" role="button" data-lan="${lan}"><svg t="1729820704088" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="14901" width="30" height="30"><path d="M927.5 161.4L786.9 93.8l-43.5-20.9c-5.4-2.6-11-4.2-16.7-5h-0.2c-0.6-0.1-1.2-0.2-1.8-0.2-16.3-1.6-32.9 4-44.9 16.1 0.8-0.8 1.7-1.6 2.6-2.3-0.8 0.7-1.7 1.5-2.5 2.3L327.3 405.1 173.7 288.7c-14.3-10.9-34.4-10-47.7 2.1l-49.2 44.7c-0.9 0.8-1.7 1.6-2.5 2.5-13.8 15.2-12.7 38.8 2.6 52.6L210.1 512 76.9 633.4c-15.2 13.8-16.4 37.4-2.6 52.6 0.8 0.9 1.6 1.7 2.5 2.5l49.2 44.7c13.3 12.1 33.3 13 47.6 2.1l153.6-116.5 352.7 321.6c16.7 16.7 42.2 21.1 63.5 10.8l184.2-88.5c19.4-9.3 31.7-28.9 31.7-50.3V211.7c0-21.5-12.4-41.1-31.8-50.3zM468.1 512l267.5-202.9v405.8L468.1 512z" fill="#ffffff" p-id="14902"></path></svg></a>
-      <a href="javascript:" class="nav_a open-site" title="Copy Website URL" role="button" data-lan="${lan}"><svg t="1729820741115" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="16916" width="30" height="30"><path d="M536.704 162.944a229.312 229.312 0 0 1 324.352 324.352L755.84 595.2a42.688 42.688 0 0 1-61.056-59.584l105.728-108.416a144 144 0 1 0-203.648-203.648l-0.384 0.384-108.032 105.344a42.688 42.688 0 0 1-59.52-61.056l107.776-105.216zM702.08 321.92c16.64 16.64 16.64 43.648 0 60.288l-320 320a42.688 42.688 0 0 1-60.288-60.352l320-320c16.64-16.64 43.648-16.64 60.288 0z m-373.696 106.24c16.896 16.512 17.28 43.52 0.768 60.352L223.488 596.864a144 144 0 1 0 203.648 203.648l0.384-0.384 108.032-105.344a42.688 42.688 0 1 1 59.52 61.056l-107.776 105.152a229.312 229.312 0 0 1-324.288-324.288L268.16 428.8a42.688 42.688 0 0 1 60.288-0.768z" fill="#ffffff" p-id="16917"></path></svg></a>
     </div>
     <ul>
       ${data
@@ -287,8 +286,6 @@ const createContent = (
   const discardCode = document.querySelectorAll(".discard-code");
   const mergeCodes = document.querySelectorAll(".merge-code");
   const deleteFiles = document.querySelectorAll(".delete");
-  const vsCodes = document.querySelectorAll(".vs-code");
-  const openSite = document.querySelectorAll(".open-site");
 
   new Tips($(".jsRTips"), {
     align: "top",
@@ -673,43 +670,44 @@ const createContent = (
         });
     };
   });
-  vsCodes.forEach((item) => {
-    item.onclick = () => {
-      const { lan } = item.dataset;
-      fetch("/open-vscode", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          lan,
-        }),
-      });
-    };
-  });
-  openSite.forEach((item) => {
-    item.onclick = () => {
-      const { lan } = item.dataset;
-      fetch("/open-site", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          lan,
-        }),
-      })
-        .then((res) => {
-          return res.json();
-        })
-        .then((res) => {
-          if (res?.data) {
-            copyToClipboard(res.data);
-          }
-        });
-    };
-  });
 };
+
+vsCodes.forEach((item) => {
+  item.onclick = () => {
+    const { lan } = item.dataset;
+    fetch("/open-vscode", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        lan,
+      }),
+    });
+  };
+});
+openSite.forEach((item) => {
+  item.onclick = () => {
+    const { lan } = item.dataset;
+    fetch("/open-site", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        lan,
+      }),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        if (res?.data) {
+          copyToClipboard(res.data);
+        }
+      });
+  };
+});
 
 function copyToClipboard(text) {
   if (navigator.clipboard && navigator.clipboard.writeText) {
