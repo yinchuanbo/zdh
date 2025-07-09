544d74ce73ed7eb53ff8110dd990b9f222ad41da
YinHao <1469786443@qq.com>
2025-07-09 10:34:22 +0800

    editor 版本更新

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1029,7 +1029,7 @@ function setEditor(path = "", modifiedText = "", originalText = "") {
 
   require.config({
     paths: {
-      vs: "https://unpkg.com/monaco-editor@0.47.0/min/vs",
+      vs: "https://unpkg.com/monaco-editor@0.34.1/min/vs",
     },
   });
 
@@ -1178,7 +1178,7 @@ const diffHTML = function (
     jsonEditor = null;
     require.config({
       paths: {
-        vs: "https://unpkg.com/monaco-editor@0.47.0/min/vs",
+        vs: "https://unpkg.com/monaco-editor@0.34.1/min/vs",
       },
     });
     require(["vs/editor/editor.main"], function () {
